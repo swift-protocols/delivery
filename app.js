b5e9f881cd145c8365e5d9bdc90660800a68d464
swift-protocols <138299442+swift-protocols@users.noreply.github.com>
2023-07-14 04:15:00 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const requestDiv = document.getElementById("requestdiv");
-const requestText = document.getElementById("requesttext");
+const navToggle = document.querySelector(".nav-toggle");
+const headerNav = document.querySelector(".header-nav");
 
-requestDiv.addEventListener(onmouseover, function(){
-    requestText.innnerHTML = `<h3 class="desc-small">
-     hey hey hey
- </h3>`;
+navToggle.addEventListener("click", function(){
+    headerNav.classList.toggle("show-nav");
 })
+
+const header = document.querySelector(".header");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function(){
+    const scrollHeight = window.pageYOffset;
+    const headerHeight = header.getBoundingClientRect().height;
+    if (scrollHeight > headerHeight) {
+        header.classList.add("fixed-nav")
+    }  else{
+        header.classList.remove("fixed-nav");
+    }
+    if (scrollHeight > 500) {
+        topLink.classList.add("scroll-link")
+    } else {
+        topLink.classList.remove("scroll-link")
+    }
+})
+
